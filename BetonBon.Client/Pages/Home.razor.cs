624f9d26db91e2b9f5723f5036f3a38ce6a201ad
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-05-04 11:05:36 +0200

    Split Home into separate razor, css, and cs files.

---
+++ BetonBon.Client/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BetonBon.Client.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; } = default!;
+
+        private enum TimerState { NotStarted, Running }
+        private TimerState _timerState = TimerState.NotStarted;
+        private DateTime? _startedAt = null;
+        private Timer? _timer;
+
+    }
+}
