38fe0de5e3277e3ad856d4169ab74f1af09df146
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-05-04 13:29:29 +0200

    Added stopwatch to TimerCard.
    Stopwatch can be started, stopped, and restarted.

--- BetonBon.Client/Pages/Home/Home.razor.cs
+++ BetonBon.Client/Pages/Home/Home.razor.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace BetonBon.Client.Pages.Home
+﻿namespace BetonBon.Client.Pages.Home
 {
     public partial class Home
     {
-        [Inject]
-        public NavigationManager Navigation { get; set; } = default!;
+
     }
 }
--- BetonBon.Client/Pages/Home/TimerCard.razor.cs
+++ BetonBon.Client/Pages/Home/TimerCard.razor.cs
@@ -1,10 +1,56 @@
-﻿namespace BetonBon.Client.Pages.Home
+﻿using System.Diagnostics;
+
+namespace BetonBon.Client.Pages.Home
 {
-    public partial class TimerCard
+    public partial class TimerCard : IAsyncDisposable
     {
         private enum TimerState { NotStarted, Running }
         private TimerState _timerState = TimerState.NotStarted;
-        private DateTime? _startedAt = null;
-        private Timer? _timer;
+        private Stopwatch? _stopwatch;
+
+        private PeriodicTimer? _periodicTimer;
+
+        private void StartTimer()
+        {
+            _timerState = TimerState.Running;
+            _stopwatch = new();
+            _stopwatch.Start();
+            _ = StartTicking();
+        }
+
+        private void StopTimer()
+        {
+            _timerState = TimerState.NotStarted;
+            _stopwatch?.Stop();
+            _periodicTimer?.Dispose();
+        }
+
+        private async Task StartTicking()
+        {
+            _periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+            while (await _periodicTimer.WaitForNextTickAsync())
+            {
+                StateHasChanged();
+            }
+        }
+
+        private string FormatElapsed()
+        {
+            if (_stopwatch == null) return "0s";
+
+            var ts = _stopwatch.Elapsed;
+            if (ts.TotalHours >= 1)
+                return $"{(int)ts.TotalHours}t {ts.Minutes}m {ts.Seconds}s";
+            if (ts.TotalMinutes >= 1)
+                return $"{ts.Minutes}m {ts.Seconds}s";
+            return $"{ts.Seconds}s";
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _periodicTimer?.Dispose();
+            GC.SuppressFinalize(this);
+        }
     }
 }
