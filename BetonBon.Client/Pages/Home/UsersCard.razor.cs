6737b4f3117d74b4f7a8b88037f544a7af1fcff8
mikl0w <mikkelklitgaard@live.dk>
2026-05-06 11:10:10 +0200

    Add UsersCard component and update user logic
    Added UsersCard with navigation to /users and custom styles. Integrated UsersCard into Home.razor. Cleaned up Users.razor by removing placeholder comments and adding a method to get user initials.

---
+++ BetonBon.Client/Pages/Home/UsersCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BetonBon.Client.Pages.Home
+{
+    public partial class UsersCard
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; } = default!;
+
+        private void NavigateToUsers()
+        {
+            Navigation.NavigateTo("/users");
+        }
+    }
+}
