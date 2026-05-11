b6422ef79f486bde05e8fef98f14b34ccfecd34e
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-05-11 12:18:03 +0100

    Moved JS localstore calls into LocalStorage service class to abstract away the JS.

---
+++ BetonBon.Client/Services/LocalStorage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+
+namespace BetonBon.Client.Services
+{
+    public class LocalStorage
+    {
+        private readonly IJSRuntime _js;
+
+        public LocalStorage(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task<string?> LoadAsync(string key)
+        {
+            return await _js.InvokeAsync<string?>("storage.load", key);
+        }
+
+        public async Task SaveAsync(string key, string json)
+        {
+            await _js.InvokeVoidAsync("storage.save", key, json);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _js.InvokeVoidAsync("storage.remove", key);
+        }
+    }
+}
