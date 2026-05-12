d42cc8982fbac47fbe07b7183124c4f1ceff3c36
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2026-05-12 16:23:06 +0200

    Added EditUserForm component and UpdateUserModel with validation

---
+++ BetonBon.Client/Shared/ViewModels/UpdateUserModel.cs
@@ -0,0 +1,18 @@
+﻿using BetonBon.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BetonBon.Client.Shared.ViewModels
+{
+    public class UpdateUserModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Brugernavn skal udfyldes")]
+        public string Username { get; set; } = string.Empty;
+
+        public string? NewPassword { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }
+    }
+}
