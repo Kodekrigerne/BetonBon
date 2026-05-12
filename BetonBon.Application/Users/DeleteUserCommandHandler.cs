179328611472d912f2a57f448d13cc2db17e05da
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2026-05-12 16:21:26 +0200

    Added DeleteUserCommand and implemented CommandHandler for DeleteUser

---
+++ BetonBon.Application/Users/DeleteUserCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using BetonBon.Application.RepositoryInterfaces;
+
+namespace BetonBon.Application.Users
+{
+    public class DeleteUserCommandHandler : ICommandHandler<DeleteUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(DeleteUserCommand command)
+        {
+            var user = await _userRepository.GetByIdAsync(command.Id);
+
+            if (user != null)
+            {
+                _userRepository.Delete(user);
+
+                await _userRepository.SaveChangesAsync();
+            }
+        }
+    }
+}
