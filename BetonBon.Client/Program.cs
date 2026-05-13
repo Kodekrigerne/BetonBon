7272c4722f001942e862e8ad7c1ad675781efe99
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2026-05-13 12:55:05 +0200

    Implemented PopupService to confirm create/delete/update User

--- BetonBon.Client/Program.cs
+++ BetonBon.Client/Program.cs
@@ -1,7 +1,7 @@
 using BetonBon.Client.Auth;
 using BetonBon.Client.RefitInterfaces;
-using Microsoft.AspNetCore.Components.Authorization;
 using BetonBon.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Refit;
@@ -34,6 +34,8 @@ namespace BetonBon.Client
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             builder.Services.AddScoped<LocalStorage>();
+            builder.Services.AddScoped<PopupService>();
+
 
             await builder.Build().RunAsync();
         }
