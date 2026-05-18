946abda3e1123e9bc911692e7025a6b345e64d71
Mikkel Dahlmann <dahlmann.mikkel@gmail.com>
2026-05-18 14:13:16 +0200

    Change CORS policy to allow any origins

--- BetonBon.API/Program.cs
+++ BetonBon.API/Program.cs
@@ -101,7 +101,7 @@ namespace BetonBon.API
 
             builder.Services.AddCors(options => options.AddPolicy("CustomPolicy", policy =>
             {
-                policy.WithOrigins(clientUrl!);
+                policy.AllowAnyOrigins();
                 policy.AllowAnyMethod();
                 policy.AllowAnyHeader();
             }));
