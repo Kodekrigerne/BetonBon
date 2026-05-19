5ce5d4bd1ee1076106766aab0293e1ccee3ad7f2
mikl0w <mikkelklitgaard@live.dk>
2026-05-19 15:20:03 +0200

    Added migration

---
+++ BetonBon.Infrastructure/Migrations/20260519130952_AddConcurrencyToUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BetonBon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConcurrencyToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Users",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Users");
+        }
+    }
+}
