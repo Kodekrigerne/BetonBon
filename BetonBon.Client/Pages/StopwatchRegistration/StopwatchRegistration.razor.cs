a21a30525f2e740c89110709240d6595a72c22e7
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-05-19 08:44:34 +0200

    Added alert popups when unable to save drafts on stopwatch registration page.

--- BetonBon.Client/Pages/StopwatchRegistration/StopwatchRegistration.razor.cs
+++ BetonBon.Client/Pages/StopwatchRegistration/StopwatchRegistration.razor.cs
@@ -25,6 +25,9 @@ namespace BetonBon.Client.Pages.StopwatchRegistration
         [Inject]
         public TimeEntryService TimeEntryService { get; set; } = null!;
 
+        [Inject]
+        public PopupService PopupService { get; set; } = null!;
+
         private bool _projectsIsVisible;
         private bool _activitiesIsVisible;
         private Guid? _selectingForDraftId;
@@ -100,7 +103,16 @@ namespace BetonBon.Client.Pages.StopwatchRegistration
 
         private async Task SaveAllocations()
         {
-            if (_allocationDrafts.Any(d => d.ProjectDTO == null || d.ActivityDTO == null)) return; //TODO: Popup, snack bar?
+            if (_allocationDrafts.Any(d => d.ProjectDTO == null || d.ActivityDTO == null))
+            {
+                await PopupService.AlertAsync("Fjern venligst drafts uden valgt projekt og aktivitet.");
+                return;
+            }
+            if (RemainingMinutes != 0)
+            {
+                await PopupService.AlertAsync("Fordel venligst al tid.");
+                return;
+            }
 
             var responses = await TimeEntryService.CreateTimeEntries(_session, _allocationDrafts);
 
