04fa9c035f9da25dd373bcca6adfc2b19a9dddd1
mikl0w <mikkelklitgaard@live.dk>
2026-05-19 11:43:49 +0200

    MainLayout and Header fix

--- BetonBon.Client/Pages/Home/Home.razor.cs
+++ BetonBon.Client/Pages/Home/Home.razor.cs
@@ -1,11 +1,7 @@
-﻿using BetonBon.Client.Auth;
-
-namespace BetonBon.Client.Pages.Home
+﻿namespace BetonBon.Client.Pages.Home
 {
     public partial class Home
     {
-
-        public bool IsDropdownVisible = false;
         public void NavigateToAdministration()
         {
             _navigation.NavigateTo("/administration");
@@ -14,21 +10,6 @@ namespace BetonBon.Client.Pages.Home
         public void OpenRegistrations()
         {
             _navigation.NavigateTo("/registration");
-        }
-
-        public void ToggleDropdown()
-        {
-            IsDropdownVisible = !IsDropdownVisible;
-        }
-
-        public async Task Logout()
-        {
-            await localStorage.RemoveAsync("bb_token");
-            await localStorage.RemoveAsync("bb_refresh_token");
-
-            var customAuthStateProvider = (CustomAuthStateProvider)AuthStateProvider;
-
-            await customAuthStateProvider.UpdateAuthenticationStatus();
         }
     }
 }
